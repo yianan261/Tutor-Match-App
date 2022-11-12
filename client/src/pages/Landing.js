9b5d0826751756048c584a546e79b6b48726a6fa
yianan261 <yian.chen0131@gmail.com>
2022-11-12 08:24:53 -0800

    changed font

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import "../assets/styles/Landing.css";
 
 function Landing() {
   return (
     <>
       <Navbar />
-      <h1>Home Page</h1>
+      <h1>Home Page Testing</h1>
     </>
   );
 }
