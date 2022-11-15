5a3b33c3b60783fc18ed585b708feda0de3482d8
yianan261 <yian.chen0131@gmail.com>
2022-11-14 20:55:00 -0800

    deleted cors from server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import tutor from "./routers/tutors.js";
 import test from "./routers/test.js";
-import cors from "cors";
+
 // require("./config/passport")(app);
 // import register from "./routes/register";
 // import login from "./routers/login";
@@ -17,7 +17,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(cors());
+
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
