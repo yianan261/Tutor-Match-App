6e2011434c9ee0caa071f49ce35654594377bfd4
yianan261 <yian.chen0131@gmail.com>
2022-11-19 00:02:45 -0500

    fixed BookModal, BookClass

--- db/myDB.js
+++ db/myDB.js
@@ -162,21 +162,40 @@ function MyMongoDB() {
     }
   };
 
+  /**
+   * Yian
+   * @param {String} user email
+   * @returns user's schedule
+   */
+  myDB.getUserSchedule = async (_user) => {
+    let client;
+    try {
+      client = new MongoClient(url);
+      const userCol = client.db(DB_NAME).collection(USER_COLLECTION);
+      const options = {
+        projection: { salt: 0, hash: 0 },
+      };
+      return await userCol.findOne({ user: _user }, options);
+    } finally {
+      client.close();
+    }
+  };
+
   /**Yian
-   * Updates new booking to "schedule" for user, if "schedule" doesn't exist one will be created
+   * Updates new bookings to "schedule" for user, if "schedule" doesn't exist one will be created
    * @param {string} _user (ID)
-   * @param {Map} _booking
+   * @param {array} _booking
    */
   myDB.createBooking = async (_user, _booking) => {
     let client;
     try {
       console.log("show Booking DB", _booking);
+      
       client = new MongoClient(url);
       const userCol = client.db(DB_NAME).collection(USER_COLLECTION);
-      // const options = { projection: { reviews: 0 } };
       return await userCol.updateOne(
         { user: _user },
-        { $push: { schedule: _booking } }
+        { $push: { schedule: { $each: _booking } } }
       );
     } finally {
       client.close();
