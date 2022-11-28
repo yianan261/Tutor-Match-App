3d1a884735f2d6c23917f2b861b5b45071ff6369
amandaay <amandaay.aysma@gmail.com>
2022-11-28 14:16:14 -0800

    fixed controller, moved router stuff

--- routers/editProfile.js
+++ routers/editProfile.js
@@ -1,13 +1,10 @@
 import express from "express";
 import {
-  updateProfile,
+  updateProfile, uploadPic,
   delPic,
   retrieveProfileInfo,
   redirectEditProfile,
 } from "../controllers/editProfile.js";
-import myDB from "../db/myDB.js";
-import fs from "fs";
-import cloudinary from "../controllers/utils/cloudinary.js";
 import upload from "../controllers/utils/multer.js";
 
 const router = express.Router();
@@ -21,19 +18,7 @@ router.post("/api/profile/editProfile", updateProfile);
 /**
  * profile pics posting to DB
  */
-router.post("/api/upload", upload.single("img"), async (req, res) => {
-  try {
-    if (req.file) {
-      const cloudRes = await cloudinary.uploader.upload(req.file.path);
-      await myDB.updatesPic(req.session.passport.user, cloudRes.url);
-      fs.unlinkSync(req.file.path);
-      res.redirect("/profile/EditProfile");
-    }
-  } catch (err) {
-    res.status(400).send({ err: `There is an ${err}` });
-    fs.unlinkSync(req.file.path);
-  }
-});
+router.post("/api/upload", upload.single("img"), uploadPic);
 
 /**
  * del profile pic in cloudinary
