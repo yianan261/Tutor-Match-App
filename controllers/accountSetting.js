565907d6c445aa8680d87e584bed100c0716ea39
amandaay <amandaay.aysma@gmail.com>
2022-12-06 22:05:50 -0800

    fixed del acc

--- controllers/accountSetting.js
+++ controllers/accountSetting.js
@@ -3,24 +3,29 @@ import cloudinary from "./utils/cloudinary.js";
 
 /**
  * Amanda Au-Yeung
- * function to delete account from DB 
+ * function to delete account from DB
  * and delete profile pic from cloudinary
  * @param {obj} req
  * @param {obj} res
  */
 export const delProfile = async (req, res) => {
-    let id = req.query.id;
-    let user;
-    let public_id; // in cloudinary
-    try {
-      if (id) {
-        user = await myDB.getUsersById(id);
+  let id = req.params.id;
+  console.log("req.params.id", req.params.id);
+  let user;
+  let public_id; // in cloudinary
+  try {
+    if (id) {
+      user = await myDB.getUsersById(id);
+      if (user.pic) {
         public_id = user.pic.split("/").pop().split(".")[0];
         await cloudinary.uploader.destroy(public_id);
-        await myDB.deleteUser(id);
-        res.status(200).json({ message: "Sorry to see you leave:(" });
       }
-    } catch (err) {
-      res.status(400).send({ err: `There is an ${err} when you delete your account.` });
+      await myDB.deleteUser(id);
+      res.status(200).json({ message: "Sorry to see you leave:(" });
     }
+  } catch (err) {
+    res
+      .status(400)
+      .send({ err: `There is an ${err} when you delete your account.` });
   }
+};
